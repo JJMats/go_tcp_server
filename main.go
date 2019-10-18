ce5bd1464bff3c8b866185e681a834af8cb57b2f
J2Tuner <Jeremy@KroyerRacingEngines.com>
2019-10-18 14:41:44 -0700

    feat: Added eth0 connection Configured listener to receive from eth0 ip address

--- main.go
+++ main.go
@@ -13,25 +13,69 @@ const (
 )
 
 func main() {
-	// Listen for incoming connections
-	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+	// Get interfaces
+	var interfaceName = "eth0"
+	var iface, err = net.InterfaceByName(interfaceName)
 	if err != nil {
-		fmt.Println("Error listening:", err.Error())
+		fmt.Println("Could not get interface ", interfaceName, ": ", err.Error())
+		os.Exit(1)
+	}
+	//fmt.Println("Interface By Name: ", iface)
+
+	/*
+		ifaces, err := net.Interfaces()
+		if err != nil {
+			fmt.Println("Could not get interfaces: ", err.Error())
+		}
+	*/
+
+	var ip net.IP
+
+	// Loop through interfaces for addresses
+	//for _, i := range ifaces {
+	addrs, err := iface.Addrs()
+	if err != nil {
+		fmt.Println("Error getting addresses: ", err.Error())
 		os.Exit(1)
 	}
 
-	// CLose the listener when the application closes
-	defer l.Close()
-	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
-	for {
-		// Listen for an incoming connection
-		conn, err := l.Accept()
+	for _, addr := range addrs {
+		var iip net.IP
+		//fmt.Println("Addr: ", addr)
+		switch v := addr.(type) {
+		case *net.IPNet:
+			iip = v.IP
+		case *net.IPAddr:
+			iip = v.IP
+		}
+		if iip.To4() != nil {
+			ip = iip
+			fmt.Println("Interface: ", iface.Name, "; IP Address: ", iip)
+		}
+	}
+
+	if ip.To4() != nil {
+
+		// Listen for incoming connections
+		l, err := net.Listen(CONN_TYPE, ip.String()+":"+CONN_PORT)
 		if err != nil {
-			fmt.Println("Error accepting: ", err.Error())
+			fmt.Println("Error listening:", err.Error())
+			os.Exit(1)
 		}
 
-		// Handle connections in a new goroutine
-		go handleRequest(conn)
+		// CLose the listener when the application closes
+		defer l.Close()
+		fmt.Println("Listening on " + ip.String() + ":" + CONN_PORT)
+		for {
+			// Listen for an incoming connection
+			conn, err := l.Accept()
+			if err != nil {
+				fmt.Println("Error accepting: ", err.Error())
+			}
+
+			// Handle connections in a new goroutine
+			go handleRequest(conn)
+		}
 	}
 }
 
@@ -47,6 +91,7 @@ func handleRequest(conn net.Conn) {
 	}
 
 	fmt.Println("Request length: ", reqLen)
+	fmt.Println(buf)
 
 	// Send a response back to the person contacting us
 	conn.Write([]byte("Message received."))
